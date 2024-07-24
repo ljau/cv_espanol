1722d63474abd20f7a18b554fd0604a2881207a3
Luis Angel Jauregui Flores <luis.jauregui.fl@gmail.com>
2024-07-24 09:29:34 -0600

    Updates

--- src/data/MainSectionData/ExperienceData.js
+++ src/data/MainSectionData/ExperienceData.js
@@ -8,6 +8,17 @@ export const ExperienceData = {
     icon: MdWork,
     list: [
       {
+        position: "Desarrllador Web Front-end",
+        enterprise: "NAIVE labs, Colima",
+        time: "Jun 2024 - Temporal",
+        description:
+          "Desarrollo de aplicaciones web para manejo de datos de usuarios, clientes, materiales y citas.",
+        tasks: [
+          "Desarrollo de aplicaciones web progresivas y componentes reutilizables usando React.js.",
+          "Consumo de APIs REST para alimentar aplicaciones.",
+        ],
+      },
+      {
         position: "Monitorista de sistema de radio y televisión",
         enterprise: "Instituto Nacional Electoral, Colima",
         time: "Feb 2024 - Jun 2024",
@@ -23,16 +34,15 @@ export const ExperienceData = {
         enterprise: "Freelance, Colima",
         time: "2023",
         description:
-              "Desarrollo de aplicaciones web para pequeñas empresas, para gestionar proveedores, clientes, materiales y fechas de entrega.",
-            tasks: [
-              "Desarrollo de aplicaciones web progresivas usando React.js.",
-              "Desarrollo y diseño de la interfaz de usuario.",
+          "Desarrollo de aplicaciones web para pequeñas empresas, para gestionar proveedores, clientes, materiales y fechas de entrega.",
+        tasks: [
+          "Desarrollo de aplicaciones web progresivas usando React.js.",
+          "Desarrollo y diseño de la interfaz de usuario.",
         ],
       },
       {
         position: "Desarrollador Web Front-end",
-        enterprise:
-          "Tata Consultancy Services, Zapopan Jalisco",
+        enterprise: "Tata Consultancy Services, Zapopan Jalisco",
         time: "Sep 2022 - Sep 2023",
         description:
           "Desarrollo, creación y modelado de componentes en React.js para aplicaciones relacionadas con servicios bancarios.",
@@ -43,8 +53,7 @@ export const ExperienceData = {
       },
       {
         position: "Administrador de Sistemas",
-        enterprise:
-          "Tata Consultancy Services, Zapopan Jalisco",
+        enterprise: "Tata Consultancy Services, Zapopan Jalisco",
         time: "Ene 2019 - Sep 2022",
         description:
           "Administración de sistemas operativos Unix en máquinas virtuales y físicas.",
